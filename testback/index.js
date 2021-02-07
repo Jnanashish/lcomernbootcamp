7080778aa331bd4de2bd7397e183504d25fee8cf
Jnanashish <jhandique1999@gmail.com>
2021-02-07 21:54:56 +0530

    Added projbackend file

--- testback/index.js
+++ testback/index.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const app = express();
+
+const port = 3000;
+
+app.get("/", (req,res) => {
+    return res.send("Hello world")
+})
+app.get("/signup", (req,res) => {
+    return res.send("This is sign up")
+})
+
+
+app.listen(port, () => {console.log(`Server is running on ${port}`)});
